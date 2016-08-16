3e9db45826a2d597bff7c0f4028610344c9b592e
Bryan Ayllon <bryanayllonroa@gmail.com>
2016-08-16 16:14:41 -0500

    DroneVI
    App for drone

---
+++ FPVDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  DroneVI
+//
+//  Created by BryanAyllon on 8/16/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FPVDemo/DJICameraViewController.h
@@ -0,0 +1,12 @@
+//
+//  DJICameraViewController.h
+//  DroneVI
+//
+//  Created by BryanAyllon on 8/16/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJICameraViewController : UIViewController
+
+@end
